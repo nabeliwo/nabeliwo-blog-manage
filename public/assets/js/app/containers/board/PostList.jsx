a36e190242f03c36f29a38e5c73e7fb7bd11745b
nabeliwo <hiroki.c.watanabe@gmail.com>
2016-01-21 02:30:55 +0900

    postListのview作成

--- public/assets/js/app/containers/board/PostList.jsx
+++ public/assets/js/app/containers/board/PostList.jsx
@@ -7,14 +7,147 @@ export default class PostList extends Component {
     return (
       <div>
         <p className="board__title">記事の管理</p>
+
         <div className="u-cf">
-          <div className="p-PostList__sort u-fr">
-            <select className="p-PostList__sort__select">
-              <option value="diary">Diary(2)</option>
-              <option value="web">Web(2)</option>
-            </select>
+          <div className="p-postList__sort u-fr">
+            <div className="c-select c-select--small c-select--default">
+              <select className="c-select__inner">
+                <option value="all">Category</option>
+                <option value="diary">Diary(2)</option>
+                <option value="web">Web(2)</option>
+              </select>
+            </div>
+
+            <form className="p-postList__search">
+              <div className="p-postList__search__txt">
+                <input type="text" className="c-input c-input--middle" placeholder="記事を検索"/>
+              </div>
+              <div className="p-postList__search__btn c-btn c-btn--default u-hover">検索</div>
+            </form>
           </div>
         </div>
+
+        <ul className="p-postList__tab">
+          <li><a className="p-postList__tab__link is-active" href="">公開</a></li>
+          <li><a className="p-postList__tab__link" href="">下書き</a></li>
+          <li><a className="p-postList__tab__link" href="">すべて</a></li>
+        </ul>
+
+        <div className="p-postList__check u-cf">
+          <div className="u-fl">
+            <div className="c-select c-select--small c-select--default">
+              <select className="c-select__inner">
+                <option value="">チェックした記事にカテゴリーを追加</option>
+                <option value="diary">Diary(2)</option>
+                <option value="web">Web(2)</option>
+              </select>
+            </div>
+          </div>
+          <div className="u-fr">
+            <div className="p-postList__check__btn c-btn c-btn--alert u-hover">チェックした記事を削除</div>
+          </div>
+        </div>
+
+        <table className="c-table">
+          <thead>
+            <tr>
+              <td>
+                <label className="c-checkBox c-checkBox--small">
+                  <input className="c-checkBox__check" type="checkbox"/>
+                  <span className="c-checkBox__txt"></span>
+                </label>
+              </td>
+              <td>タイトル</td>
+              <td>カテゴリー</td>
+              <td>タグ</td>
+              <td>作成者</td>
+              <td>日時</td>
+              <td></td>
+            </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>
+              <label className="c-checkBox c-checkBox--small">
+                <input className="c-checkBox__check" type="checkbox"/>
+                <span className="c-checkBox__txt"></span>
+              </label>
+            </td>
+            <td><a className="c-table__link" href="">【スプラトゥーンプレイ日記4】2016年イカがんばる</a></td>
+            <td>
+              <span>Diary</span>
+              <span>Web</span>
+            </td>
+            <td>
+              <span>WiiU</span>
+              <span>splatoon</span>
+            </td>
+            <td>nabeliwo</td>
+            <td>2016/1/20 23:19:07</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>
+              <label className="c-checkBox c-checkBox--small">
+                <input className="c-checkBox__check" type="checkbox"/>
+                <span className="c-checkBox__txt"></span>
+              </label>
+            </td>
+            <td><a className="c-table__link" href="">【スプラトゥーンプレイ日記4】2016年イカがんばる</a></td>
+            <td>
+              <span>Diary</span>
+              <span>Web</span>
+            </td>
+            <td>
+              <span>WiiU</span>
+              <span>splatoon</span>
+            </td>
+            <td>nabeliwo</td>
+            <td>2016/1/20 23:19:07</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>
+              <label className="c-checkBox c-checkBox--small">
+                <input className="c-checkBox__check" type="checkbox"/>
+                <span className="c-checkBox__txt"></span>
+              </label>
+            </td>
+            <td><a className="c-table__link" href="">【スプラトゥーンプレイ日記4】2016年イカがんばる</a></td>
+            <td>
+              <span>Diary</span>
+              <span>Web</span>
+            </td>
+            <td>
+              <span>WiiU</span>
+              <span>splatoon</span>
+            </td>
+            <td>nabeliwo</td>
+            <td>2016/1/20 23:19:07</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>
+              <label className="c-checkBox c-checkBox--small">
+                <input className="c-checkBox__check" type="checkbox"/>
+                <span className="c-checkBox__txt"></span>
+              </label>
+            </td>
+            <td><a className="c-table__link" href="">【スプラトゥーンプレイ日記4】2016年イカがんばる</a></td>
+            <td>
+              <span>Diary</span>
+              <span>Web</span>
+            </td>
+            <td>
+              <span>WiiU</span>
+              <span>splatoon</span>
+            </td>
+            <td>nabeliwo</td>
+            <td>2016/1/20 23:19:07</td>
+            <td></td>
+          </tr>
+          </tbody>
+        </table>
       </div>
     );
   }
